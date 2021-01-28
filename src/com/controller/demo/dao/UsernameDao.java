7673e4f9ede351bc1050fbc3d727c283c2b49a36
Administrator <Administrator@DESKTOP-C73VVDS>
2021-01-28 22:50:00 +0800

    hkj

---
+++ src/com/controller/demo/dao/UsernameDao.java
@@ -0,0 +1,9 @@
+package com.controller.demo.dao;
+
+import java.util.List;
+
+import com.controller.demo.domain.Username;
+
+public interface UsernameDao {
+	public List<Username> findByUsername(String username);
+}
---
+++ src/test.java
@@ -0,0 +1,12 @@
+import com.controller.demo.dao.imp.UsernameDaoJdbcImp;
+import com.controller.demo.service.imp.UsernameServiceImp;
+
+public class test {
+
+	public static void main(String[] args) {
+		UsernameServiceImp a=new UsernameServiceImp();
+		System.out.println(a.checkUsername("A12"));
+
+	}
+
+}
